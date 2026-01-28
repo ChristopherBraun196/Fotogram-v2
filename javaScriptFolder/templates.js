bde2103a70690d4c46aea9f72d0971c83fd0eeac
Chrito196 <Christopher-Braun@outlook.com>
2026-01-28 20:10:38 +0100

    ++IMG / SCRIPT/ FONT / CSS

--- javaScriptFolder/templates.js
+++ javaScriptFolder/templates.js
@@ -1,10 +1,10 @@
-function getPictureTemplate(i) {
-  let pic = pictureBookOne[i];
-  return `  
-    <div class="mb-4 break-inside-avoid" onclick="loadGallery(${i})">
-        <img class="w-full aspect-square object-cover rounded-lg shadow-md cursor-pointer hover:scale-105 transition-transform" 
+
+function getPictureTemplate(pic, i, bookName) {
+  return `   
+    <button class="picture-button" onclick="openImage('${bookName}', ${i})">
+        <img class="pictureGallery" 
              src="${pic.image}" 
              alt="${pic.name}">
-    </div>
+    </button>
   `;
 }
--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let currentIndex = 0;
 
 function init() {
-loadPictures()
+ loadGallery()
 }
 
 
-function loadPictures(){
-    const container = document.getElementById('picturesOne')
-    container.innerHTML= "";
-    
-    for (let i = 0; i < pictureBookOne.length; i++) {
-        container.innerHTML += getPictureTemplate(i);
-        
-    }
+function loadPictures(pictureBook, containerID) {
+  let container = document.getElementById(containerID);
+  container.innerHTML = "";
+
+  for (let i = 0; i < pictureBook.length; i++) {
+    const picture = pictureBook[i];
+    container.innerHTML += getPictureTemplate(picture, i);
+  }
+}
+
+function loadGallery() {  
+  loadPictures(pictureBookOne, "picturesOne", "pictureBookOne");
+  loadPictures(pictureBookTwo, "picturesTwo", "pictureBookTwo");
+  loadPictures(pictureBookThree, "picturesThree", "pictureBookThree");
 }
