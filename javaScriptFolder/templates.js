b6a5b255e51437ef55253d8a2073e6f6314ae706
Chrito196 <Christopher-Braun@outlook.com>
2026-01-30 11:28:20 +0100

    add dialog ++ footer (basic) and optimate js

--- javaScriptFolder/templates.js
+++ javaScriptFolder/templates.js
@@ -1,4 +1,3 @@
-
 function getPictureTemplate(pic, i, bookName) {
   return `   
     <button class="picture-button" onclick="openImage('${bookName}', ${i})">
@@ -7,4 +6,14 @@ function getPictureTemplate(pic, i, bookName) {
              alt="${pic.name}">
     </button>
   `;
+}
+
+function openThisPicture(pic) {
+  return `
+    <div class="dialog-content">
+      <img src="${pic.image}" alt="${pic.name}" style="max-width: 100%;">
+      <p>${pic.name}</p>
+      <button onclick="document.getElementById('picturesDialogOne').close()">Schließen</button>
+    </div>
+  `; 
 }
--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+const allBooks = {
+  pictureBookOne: pictureBookOne,
+  pictureBookTwo: pictureBookTwo,
+  pictureBookThree: pictureBookThree,
+};
+
 let currentIndex = 0;
 
 function init() {
- loadGallery()
+  loadGallery();
 }
 
-
-function loadPictures(pictureBook, containerID) {
+function loadPictures(pictureBook, containerID, bookName) {
   let container = document.getElementById(containerID);
   container.innerHTML = "";
 
   for (let i = 0; i < pictureBook.length; i++) {
     const picture = pictureBook[i];
-    container.innerHTML += getPictureTemplate(picture, i);
+    container.innerHTML += getPictureTemplate(picture, i, bookName);
   }
 }
 
-function loadGallery() {  
+function openImage(bookName, index) {
+  const dialog = document.getElementById("picturesDialogOne");
+
+  const pic = allBooks[bookName][index];
+  dialog.innerHTML = openThisPicture(pic);
+
+  dialog.showModal();
+}
+
+function loadGallery() {
   loadPictures(pictureBookOne, "picturesOne", "pictureBookOne");
   loadPictures(pictureBookTwo, "picturesTwo", "pictureBookTwo");
   loadPictures(pictureBookThree, "picturesThree", "pictureBookThree");
